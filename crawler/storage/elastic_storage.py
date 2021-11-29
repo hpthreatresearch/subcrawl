6719e7edcd82df44e88c00a93df1f0b1bb60afcc
Josh Stroschein <josh@m9development.com>
2021-11-28 20:52:11 -0800

    attempts to define elastic field names along ecs standards, adds ability to save htttp response content to disk

--- crawler/storage/elastic_storage.py
+++ crawler/storage/elastic_storage.py
@@ -1,7 +1,8 @@
 # © Copyright 2021 HP Development Company, L.P.
-import json
-import pprint
-import warnings
+import os
+import zipfile
+import io
+from datetime import datetime
 from urllib.parse import urlparse
 from re import subn
 from elasticsearch import Elasticsearch, helpers
@@ -16,10 +17,14 @@ class ElasticStorage(DefaultStorage):
     logger = None
     es = None
     index = None
+    archive_location = None
+    archive_content = False
 
     def __init__(self, config, logger):
         self.cfg = config
         self.logger = logger
+        self.archive_location = SubCrawlHelpers.get_config(self.cfg,'elasticsearch', 'archive_log_location')
+        self.archive_content = SubCrawlHelpers.get_config(self.cfg, 'elasticsearch', 'archive_response_content')
         self.index = SubCrawlHelpers.get_config(self.cfg, 'elasticsearch', 'index')
 
         try:
@@ -31,6 +36,11 @@ class ElasticStorage(DefaultStorage):
             if self.es.indices.exists(self.index) == False:
                 self.logger.debug('[ELASTIC] Index did not exist, creating: ' + self.index)
                 self.es.indices.create(index=self.index)
+
+            if self.archive_content:
+                if not os.path.isdir(self.archive_location):
+                    os.mkdir(self.archive_location)
+                    self.logger.debug('[ELASTIC] Response content being saved, log created at: ' + self.archive_location)
             
         except Exception as e:
             self.logger.error('[ELASTIC] Problem connecting to Elastic: ' + str(e))
@@ -38,6 +48,24 @@ class ElasticStorage(DefaultStorage):
 
     def load_scraped_domains(self):
         return []
+
+    def normalize_field_name(self, field_name):
+        return field_name.replace(' ','-').replace('-','_').lower()
+
+    def store_content(self, content_buffer, file_name):
+
+        try:
+            tmp_buffer = io.BytesIO()
+
+            with zipfile.ZipFile(tmp_buffer, mode='w',compression=zipfile.ZIP_DEFLATED) as zip_file:
+                zip_file.writestr('http.response.payload', str.encode(content_buffer,'utf-8'))
+
+            with open(self.archive_location + file_name,'wb') as tmp_zip:
+                tmp_zip.write(tmp_buffer.getvalue())
+
+        except Exception as ex:
+            self.logger.error('[ELASTIC] Problem adding data: ' + str(ex))
+
 
     def store_result(self, result_data):
         data = {}
@@ -49,31 +77,35 @@ class ElasticStorage(DefaultStorage):
                     parsed_url = urlparse(url_content['url'])
 
                     data = {
-                        'url': url_content['url'],
-                        'scheme': parsed_url.scheme,
-                        'netloc': parsed_url.netloc,
-                        'path': parsed_url.path,
-                        'params': parsed_url.params,
-                        'query': parsed_url.query,
-                        'fragment': parsed_url.fragment,
-                        'scraped_on': url_content['scraped_on'],
-                        'sha256': url_content['sha256'],                        
-                        'content_magic': url_content['content_type'],
-                        'signature': url_content['signature'],
-                        'title': url_content['data']['title'],
-                        'response_content': url_content['data']['text'],
-                        'status_code': url_content['data']['resp']['status_code'],
+                        'http.request.url': url_content['url'],
+                        'http.request.scheme': parsed_url.scheme,
+                        'http.request.netloc': parsed_url.netloc,
+                        'http.request.path': parsed_url.path,
+                        'http.request.params': parsed_url.params,
+                        'http.request.query': parsed_url.query,
+                        'http.request.fragment': parsed_url.fragment,
+                        'crawled_on': url_content['scraped_on'],
+                        'http.response.body.content.sha256': url_content['sha256'],                        
+                        'http.response.body.content_magic': url_content['content_type'],
+                        'http.signature': url_content['signature'],
+                        'http.response.title': url_content['data']['title'],
+                        'http.response.status_code': url_content['data']['resp']['status_code'],
                     }
 
                     for header in url_content['data']['resp']['headers']:
-                        data[header] = url_content['data']['resp']['headers'][header]
+                        data['http.response.header.' + self.normalize_field_name(header)] = url_content['data']['resp']['headers'][header]
 
-                    for module in url_content["modules"]:
-                        if len(url_content["modules"][module]) > 0:
-                            if module == "YARAProcessing":
-                                data["yara_results"] = url_content["modules"][module]["matches"]
+                    for module in url_content['modules']:
+                        if len(url_content['modules'][module]) > 0:
+                            if module == 'YARAProcessing':
+                                data['yara_results'] = url_content['modules'][module]['matches']
+
+                    if self.archive_content:
+                        tmp_dt = datetime.strptime(url_content['scraped_on'][:-7], '%Y-%m-%dT%H:%M:%S')
+                        self.store_content(url_content['data']['text'],str(int(tmp_dt.timestamp())) + '_' + url_content['sha256'])
+
                     doc_list.append(data)
-   
+
             helpers.bulk(
                 self.es,
                 doc_list,
