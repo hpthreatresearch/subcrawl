0c71fc9849f833ac55707d4d761b6b3e58b094c5
Josh Stroschein <josh@m9development.com>
2023-01-11 18:31:56 -0600

    fixes null object error when beautifulsoup is unable to parse the returned http response

--- crawler/subcrawl.py
+++ crawler/subcrawl.py
@@ -255,7 +255,7 @@ def scrape_manager(data):
     init_pages = domain_urls
     process_processing_modules = processing_modules
 
-    logger.debug("Starting down path... " + domain_urls[0])
+    logger.debug("[ENGINE] Starting down path... " + domain_urls[0])
 
     result_dicts = list()
     for url in domain_urls:
@@ -307,15 +307,20 @@ def scrape(start_url, s_data):
             if response_size_ok:
                 content = ctt.getvalue()
                 signature = ""
+                title = None
+                bs = None
                 content_magic = "NONE"
-                bs = BeautifulSoup(str(content), "html.parser")
+                try:
+                    bs = BeautifulSoup(str(content), "html.parser")
+                    title = bs.find('title')
+                except:
+                    bs = None
                 content_magic = magic.from_buffer(content).lower()
-                title = bs.find('title')
                 module_results = {}
                 if title is not None and \
                     "index of" in title.get_text().lower() \
                         and bs is not None:
-                    
+
                     for link in bs.find_all('a'):
                         if link.has_attr('href'):
                             href = link.attrs['href']
@@ -326,7 +331,7 @@ def scrape(start_url, s_data):
                                     and not next_page.lower().endswith(tuple(SubCrawlHelpers.get_config(process_cfg, "crawler", "ext_exclude"))):
                                     logger.debug("[ENGINE] Discovered: " + next_page)
                                     crawl_pages.append(next_page)
-                                    scrape(next_page, s_data)
+                                    scrape(next_page, s_data)                
                 else:
                     for p_module in process_processing_modules:
                         mod_res = p_module.process(start_url, content)
